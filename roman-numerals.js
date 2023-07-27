a30f7d880b1a03bbe859e9c78435b8fc0b60ceff
Tefo-K <tefokeabetswe4@gmail.com>
2023-07-27 08:55:26 +0100

    fix: updated the function implementation to be able to accurately convert numbers to their equivalent roman numeral.

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,4 +1,26 @@
 function romanNumConverter(num) {
-  if (num === 1) return "I";
+  const romanNumValues = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" },
+  ];
+
+  let result = "";
+  for (const { value, numeral } of romanNumValues) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
 }
 module.exports = romanNumConverter;
